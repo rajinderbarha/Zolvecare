d5af860a5e44d061d918e3c91075e615b8ae6784
rajinderbarha <rajinderbarha@gmail.com>
2024-01-24 15:17:43 +0530

    navbar update

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -37,23 +37,19 @@ function NavBar() {
 
               <SheetTrigger
                 type="button"
-                className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+                
+                className="-m-2.5 inline-flex items-center justify-center rounded-md text-gray-700"
               >
                 <span className="sr-only">Open main menu</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg>
+              
+                <Image
+                style={{width:"48px", height:"48px"}}
+                  className="h-8 w-auto"
+                  src="/Image/Menu_Nav.svg"
+                  alt="logos"
+                  width={52}
+                  height={52}
+                />
               </SheetTrigger>
             </div>
             <div className="hidden lg:flex lg:gap-x-16">
